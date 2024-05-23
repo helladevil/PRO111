b784b9420030ee70c6847b617ea6342f85b561e7
helladevil <127803793+helladevil@users.noreply.github.com>
2024-05-23 20:48:59 -0300

    Add files via upload

---
+++ move.py
@@ -0,0 +1,8 @@
+import os
+import shutil
+ 
+from_dir= 'C:/Users/Windows/Documents/mi primer python/PRO112/kirara.png'
+to_dir='C:/Users/Windows/Documents/mi primer python/PRO111/images'
+
+list_of_files= os.listdir(from_dir)
+print(list_of_files)
